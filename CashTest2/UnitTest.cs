721b95004bc78727bc4a90000555ab905324c678
joe <joe@changingtec.com>
2019-07-27 15:20:02 +0800

    加入專案檔案。

---
+++ CashTest2/UnitTest.cs
@@ -0,0 +1,16 @@
+﻿
+using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace CashTest2
+{
+    [TestClass]
+    public class BudgetService
+    {
+        [TestMethod]
+        public void TestMethod1()
+        {
+
+        }
+    }
+}
